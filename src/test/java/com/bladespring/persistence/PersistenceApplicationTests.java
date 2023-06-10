0be861ffe4a5d1562578c714311509cd2eb3956b
Blessed Sibanda <blessedsibanda.spring@gmail.com>
2023-06-10 14:03:02 +0200

    Initialize spring boot project

---
+++ src/test/java/com/bladespring/persistence/PersistenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bladespring.persistence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersistenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
